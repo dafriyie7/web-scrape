e9d3810b146b176645e02783c745d4c8eac78918
Daniel Afriyie <dafriyie7@gmail.com>
2025-02-07 09:45:03 +0000

    Fix Puppeteer deployment

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const puppeteer = require("puppeteer");
   // Launch browser
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"], // Needed for cloud deployment
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/google-chrome",
   });
+
 
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "domcontentloaded" });
